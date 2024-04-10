0387da24e84ebe9e0e60ae74065271b2ab217930
Vibhav-Jaiswal <vibhavjaiswal9793@gmail.com>
2024-04-10 10:09:18 +0530

    complete prrofile page.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center py-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.profilePicture}
+          alt="profile"
+          className="h-24 w-24 self-center rounded-full object-cover mt-2"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="bg-slate-100 rounded-lg p-3"
+          defaultValue={currentUser.username}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          className="bg-slate-100 rounded-lg p-3"
+          defaultValue={currentUser.email}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="bg-slate-100 rounded-lg p-3"
+          defaultValue={"*******************"}
+        />
+        
+      </form>
+      <div className="text-center mt-5">
+        <span className="text-red-700 cursor-pointer ">Sign out</span>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
