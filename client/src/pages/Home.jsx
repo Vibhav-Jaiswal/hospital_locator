5e53381014a4f7daebcb62b3ca1bec42df29bcbf
Vibhav-Jaiswal <vibhavjaiswal9793@gmail.com>
2024-04-19 11:33:21 +0530

    update css in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div
-      className="flex flex-col text-center py-40"
+      className="flex flex-col items-center text-center py-40"
       style={{
         backgroundImage:
           "url(https://cdn.pixabay.com/photo/2024/04/10/00/46/hospital-8687007_640.jpg)",
@@ -13,11 +13,11 @@ const Home = () => {
         height: "92.2vh",
       }}
     >
-      <h1 className="text-3xl text-center py-4 font-semibold uppercase">
+      <h1 className="text-4xl text-center py-4 font-semibold uppercase">
         Search for hospitals nearby your location
       </h1>
-      <Link to="location">
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+      <Link to="location" className="w-fit">
+        <button className="bg-slate-700 text-white py-3 px-10 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
           Start Searching
         </button>
       </Link>
